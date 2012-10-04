1d5332d2df6677a040620a00bc7c88f61d522167
Xuanyi Chew <chewxy@gmail.com>
2012-10-04 16:38:37 +1000

    FIRST!

---
+++ examples/push-to-talk.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+import sys, zmq, socket, re, traceback
+
+HOSTING = False
+if '--host' in sys.argv:
+	HOSTING = True
+
+IP = None
+IPPattern = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+PORT = None
+for arg in sys.argv:
+	try:
+		PORT = int(arg)
+	except:
+		pass
+
+for arg in sys.argv:
+	try:
+		IP = re.match(IPPattern, arg).group()
+	except:
+		pass
+
+if not HOSTING and not IP:
+	print 'IP Address needed'
+	sys.exit()
+if not PORT:
+	print 'PORT NEEDED'
+	sys.exit()
+
+context = zmq.Context()
+pushSocket = context.socket(zmq.PUSH)
+pullSocket = context.socket(zmq.PULL)
+
+if HOSTING:
+	pushSocket.bind('tcp://*:%s' % PORT)
+	pullSocket.bind('tcp://*:%s' % (PORT + 1))
+	print ('Now Listening on: %s') % (socket.gethostbyname(socket.gethostname()) + ':' + str(PORT))
+else:
+	pullSocket.connect('tcp://%s:%s' % (IP,PORT))
+	pushSocket.connect('tcp://%s:%s' % (IP, (PORT + 1)))
+
+poller = zmq.Poller()
+poller.register(pullSocket, zmq.POLLIN)
+
+def main():
+	while True:
+		try:
+			socks = dict(poller.poll(1000))
+			msg = pullSocket.recv()
+			print '>> %s' % msg
+			continue
+		except KeyboardInterrupt:
+			i = raw_input('>')
+			pushSocket.send(i)
+
+if __name__ == '__main__':
+	main()
